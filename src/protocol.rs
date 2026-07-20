91acb46a0660128d3b9b302ce3b242075fcfba8e
Burvye <thebuffonion@gmail.com>
2026-07-20 17:45:16 -0400

    feat: inputs and marker

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,5 +1,6 @@
 use avian3d::prelude::*;
 use bevy::prelude::*;
+use serde::{Deserialize, Serialize};
 
 pub struct ProtocolPlugin;
 
@@ -8,8 +9,15 @@ impl Plugin for ProtocolPlugin {
         println!("Protocol Plugin added!"); // TODO: Remove debug logging
     }
 }
-pub enum CharacterWant {
-    Move,
-    Jump,
-    Fire,
+
+#[derive(Component, Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub struct PlayerMarker;
+/// A bunch of inputs that are sent over to the server
+/// and must be simulated in one tick.
+/// Can contain anything you want
+pub struct Inputs {
+    pub up: bool,
+    pub down: bool,
+    pub left: bool,
+    pub right: bool,
 }
